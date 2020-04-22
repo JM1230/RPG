8fa2d7e85c89aebcc3c71526cf13f51c46909313
jaakub999 <59767261+jaakub999@users.noreply.github.com>
2020-04-22 16:58:52 +0200

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,6 +1,17 @@
 import random
 
-def cube():
-    x = random.randrange(2, 13)
-    print("Wyrzucona liczba oczek:", x)
-    return x
+def cube(c):
+    if c == 1:
+        x = random.randrange(2, 13)
+        print("Wyrzucona liczba oczek:", x)
+        return x
+
+    elif c == 2:
+        x = random.randrange(2, 21)
+        print("Wyrzucona liczba oczek:", x)
+        return x
+
+    elif c == 3:
+        x = random.randrange(2, 31)
+        print("Wyrzucona liczba oczek:", x)
+        return x
