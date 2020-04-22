cd2fc58effe9c1d227f555410af8d7c9ded61fbe
jaakub999 <59767261+jaakub999@users.noreply.github.com>
2020-04-22 17:59:04 +0200

    Update person.py

--- person.py
+++ person.py
@@ -2,7 +2,7 @@ from bcolors import bcolors
 import random
 
 class Person:
-    def __init__(self, name, cl, hp, mp, atk, magic, items):
+    def __init__(self, name, cl, hp, mp, atk, power, magic, items):
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
@@ -10,6 +10,7 @@ class Person:
         self.atklow = atk - 10
         self.atkhigh = atk + 10
         self.magic = magic
+        self.power = power
         self.items = items
         self.action = ["Atak", "Umiejętności", "Przedmioty"]
         self.name = name
