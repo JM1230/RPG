2b956e17a41e34e5be89744714b60cfc551a0210
jaakub999 <59767261+jaakub999@users.noreply.github.com>
2020-04-22 17:44:55 +0200

    Update person.py

--- person.py
+++ person.py
@@ -2,7 +2,7 @@ from bcolors import bcolors
 import random
 
 class Person:
-    def __init__(self, name, class, hp, mp, atk, df, magic, items):
+    def __init__(self, name, cl, hp, mp, atk, df, magic, items):
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
@@ -14,7 +14,7 @@ class Person:
         self.items = items
         self.action = ["Atak", "Umiejętności", "Przedmioty"]
         self.name = name
-        self.class = class
+        self.class = cl
 
     def generate_damage(self):
         return random.randrange(self.atklow, self.atkhigh)
