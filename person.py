b0e96e0d4fbe20c5425aad7af7227f0da8c93bd1
jaakub999 <59767261+jaakub999@users.noreply.github.com>
2020-04-24 16:46:58 +0200

    Add files via upload

--- person.py
+++ person.py
@@ -46,7 +46,6 @@ class Person:
 
     def choose_action(self):
         i = 1
-        print("\n" + bcolors.BOLD + self.name + ":" + bcolors.ENDC)
         print(bcolors.BLUE + bcolors.BOLD + "    AKCJE:" + bcolors.ENDC)
         for item in self.action:
             print("        " + str(i) + ":", item)
@@ -58,86 +57,3 @@ class Person:
         for spell in self.magic:
             print("        " + str(i) + ":", spell.name, "(koszt:", str(spell.cost) + ")")
             i += 1
-
-    def get_enemy_stats(self):
-        hp_bar = ""
-        bar_ticks = (self.hp / self.maxhp) * 100 / 2
-
-        while bar_ticks > 0:
-            hp_bar += "█"
-            bar_ticks -= 1
-
-        while len(hp_bar) < 50:
-            hp_bar += " "
-
-        hp_string = str(self.hp) + "/" + str(self.maxhp)
-        current_hp = ""
-
-        if len(hp_string) < 11:
-            decreased = 11 - len(hp_string)
-
-            while decreased > 0:
-                current_hp += " "
-                decreased -= 1
-
-            current_hp += hp_string
-
-        else:
-            current_hp = hp_string
-
-        print("                     __________________________________________________")
-        print(bcolors.BOLD + self.name + "   " + current_hp + " |" + bcolors.RED + hp_bar + bcolors.ENDC + "|")
-
-    def get_stats(self):
-        hp_bar = ""
-        bar_ticks = (self.hp / self.maxhp) * 100 / 4
-
-        mp_bar = ""
-        mp_ticks = (self.mp / self.maxmp) * 100 / 10
-
-        while bar_ticks > 0:
-            hp_bar += "█"
-            bar_ticks -= 1
-
-        while len(hp_bar) < 25:
-            hp_bar += " "
-
-        while mp_ticks > 0:
-            mp_bar += "█"
-            mp_ticks -= 1
-
-        while len(mp_bar) < 10:
-            mp_bar += " "
-
-        hp_string = str(self.hp) + "/" + str(self.maxhp)
-        current_hp = ""
-
-        if len(hp_string) < 9:
-            decreased = 9 - len(hp_string)
-
-            while decreased > 0:
-                current_hp += " "
-                decreased -= 1
-
-            current_hp += hp_string
-
-        else:
-            current_hp = hp_string
-
-        mp_string = str(self.mp) + "/" + str(self.maxmp)
-        current_mp = ""
-
-        if len(mp_string) < 7:
-            decreased = 7 - len(mp_string)
-
-            while decreased > 0:
-                current_mp += " "
-                decreased -= 1
-
-            current_mp += mp_string
-
-        else:
-            current_mp += mp_string
-
-        print("                   _________________________                  __________ ")
-        print(bcolors.BOLD + self.name + "   " + current_hp + " |" + bcolors.GREEN + hp_bar + bcolors.ENDC + "|        " + bcolors.BOLD + current_mp + " |" + bcolors.BLUE + mp_bar + bcolors.ENDC + "|")
